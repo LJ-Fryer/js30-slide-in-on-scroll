ff187a4a2284c60cf0bb1e7ada854c4984613b3b
LJ-Fryer <ljf_aus@hotmail.com>
2025-05-20 18:35:27 +1000

    Added slide in function

--- index.js
+++ index.js
@@ -13,3 +13,25 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const slideImages = document.querySelectorAll(".slide-in");
+// Check for scroll
+function checkSlide(e) {
+  // Animate at about 50% of image showing
+  slideImages.forEach((slideImage) => {
+    const slideInAt =
+      window.scrollY + window.innerHeight - slideImage.height / 2;
+    const imageBottom = slideImage.offsetTop + slideImage.height;
+    const isHalfShown = slideInAt > slideImage.offsetTop;
+
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      slideImage.classList.add("active");
+    } else {
+      slideImage.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide)); // ("scroll", debounce(checkSlide, 500)) to adjust timing
